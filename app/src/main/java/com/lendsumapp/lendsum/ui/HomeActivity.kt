c51c7a308d62bee60735a7a4d946434b388de362
jm47895 <jordan.madrigal@gmail.com>
2020-08-07 03:35:24 -0500

    Include search, edit, and back arrow icons. Enable color change to bottom nav.

--- app/src/main/java/com/lendsumapp/lendsum/ui/HomeActivity.kt
+++ app/src/main/java/com/lendsumapp/lendsum/ui/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.lendsumapp.lendsum.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,18 +20,38 @@ class HomeActivity: AppCompatActivity(), BottomNavigationView.OnNavigationItemSe
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        navigation.setOnNavigationItemSelectedListener(this)
-
+        setUpBottomNavBarOnStart()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.profile -> Toast.makeText(this, "Profile!", Toast.LENGTH_SHORT).show()
-            R.id.messages -> Toast.makeText(this, "Messages!", Toast.LENGTH_SHORT).show()
-            R.id.add -> Toast.makeText(this, "Add!", Toast.LENGTH_SHORT).show()
-            R.id.bundles -> Toast.makeText(this, "Bundles!", Toast.LENGTH_SHORT).show()
-            R.id.services -> Toast.makeText(this, "Services!", Toast.LENGTH_SHORT).show()
+            R.id.profile -> {
+                item.isChecked = true
+                Toast.makeText(this, "profile!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.messages -> {
+                item.isChecked = true
+                Toast.makeText(this, "messages!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.search -> {
+                item.isChecked = true
+                Toast.makeText(this, "search!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.bundles -> {
+                item.isChecked = true
+                Toast.makeText(this, "bundles!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.services -> {
+                item.isChecked = true
+                Toast.makeText(this, "services!", Toast.LENGTH_SHORT).show()
+            }
         }
         return false
     }
+
+    private fun setUpBottomNavBarOnStart(){
+        navigation.selectedItemId = R.id.search
+        navigation.setOnNavigationItemSelectedListener(this)
+    }
+
 }
