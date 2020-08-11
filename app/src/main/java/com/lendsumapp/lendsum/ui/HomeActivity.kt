5bdea83f7d644c0e117af16b936658b54a325a21
jm47895 <jordan.madrigal@gmail.com>
2020-08-10 21:27:44 -0500

    Add fragments, nav graph components and edit onitem listener

--- app/src/main/java/com/lendsumapp/lendsum/ui/HomeActivity.kt
+++ app/src/main/java/com/lendsumapp/lendsum/ui/HomeActivity.kt
@@ -2,57 +2,101 @@ package com.lendsumapp.lendsum.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
+import android.os.Handler
 import android.view.MenuItem
-import android.widget.EditText
+import android.view.View
 import android.widget.Toast
+import androidx.core.os.postDelayed
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.lendsumapp.lendsum.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_home.*
 
 @AndroidEntryPoint
-class HomeActivity: AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class HomeActivity: AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
 
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        setUpBottomNavBarOnStart()
+        setupBottomNavigation()
+
+    }
+
+    private fun setupBottomNavigation() {
+
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        bottom_navigation.selectedItemId = R.id.marketplace
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottom_navigation, navHostFragment.navController)
+
+    }
+
+    fun showBottomNavigation()
+    {
+        bottom_navigation.visibility = View.VISIBLE
+    }
+
+    fun hideBottomNavigation()
+    {
+        bottom_navigation.visibility = View.GONE
+    }
+
+    private var backPressedOnce = false
+
+    override fun onBackPressed() {
+        if (navController.graph.startDestination == navController.currentDestination?.id)
+        {
+            if (backPressedOnce)
+            {
+                super.onBackPressed()
+                return
+            }
+
+            backPressedOnce = true
+            Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler().postDelayed(2000){
+                backPressedOnce = false
+            }
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.profile -> {
                 item.isChecked = true
-                Toast.makeText(this, "profile!", Toast.LENGTH_SHORT).show()
+                print("P")
             }
             R.id.messages -> {
                 item.isChecked = true
-                Toast.makeText(this, "messages!", Toast.LENGTH_SHORT).show()
+                print("Me")
             }
-            R.id.search -> {
+            R.id.marketplace -> {
                 item.isChecked = true
-                Toast.makeText(this, "search!", Toast.LENGTH_SHORT).show()
+                print("Ma")
             }
             R.id.bundles -> {
                 item.isChecked = true
-                Toast.makeText(this, "bundles!", Toast.LENGTH_SHORT).show()
+                print("B")
             }
             R.id.services -> {
                 item.isChecked = true
-                Toast.makeText(this, "services!", Toast.LENGTH_SHORT).show()
+                print("S")
             }
         }
-        return false
-    }
 
-    private fun setUpBottomNavBarOnStart(){
-        navigation.selectedItemId = R.id.search
-        navigation.setOnNavigationItemSelectedListener(this)
+        return false
     }
 
 }
---
+++ app/src/main/java/com/lendsumapp/lendsum/ui/ServicesFragment.kt
@@ -0,0 +1,25 @@
+package com.lendsumapp.lendsum.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lendsumapp.lendsum.R
+
+class ServicesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_services, container, false)
+    }
+
+}
