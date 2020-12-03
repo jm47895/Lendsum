d9cc1a4a39924780af8e66641bdb408c5b6aef43
jm47895 <jordan.madrigal@gmail.com>
2020-12-03 00:48:11 -0600

    Optimize realtimedb chat room objects by excluding info that is not needed.

--- app/src/main/java/com/lendsumapp/lendsum/data/model/User.kt
+++ app/src/main/java/com/lendsumapp/lendsum/data/model/User.kt
@@ -3,19 +3,20 @@ package com.lendsumapp.lendsum.data.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.firebase.database.Exclude
 
 @Entity(tableName = "user")
 data class User(
     @PrimaryKey @ColumnInfo(name = "userId") var userId: String,
     @ColumnInfo(name = "name") var name: String,
-    @ColumnInfo(name = "username") var username: String,
-    @ColumnInfo(name = "email") var email: String,
-    @ColumnInfo(name = "phoneNumber") var phoneNumber: String,
+    @get: Exclude @ColumnInfo(name = "username") var username: String,
+    @get: Exclude @ColumnInfo(name = "email") var email: String,
+    @get: Exclude @ColumnInfo(name = "phoneNumber") var phoneNumber: String,
     @ColumnInfo(name = "profilePicUri") var profilePicUri: String?,
-    @ColumnInfo(name = "karmaScore") var karmaScore: Int,
+    @get: Exclude @ColumnInfo(name = "karmaScore") var karmaScore: Int,
     //TODO Change to type Friend object when feature is implemented
-    @ColumnInfo(name = "friendsList") var friendList: List<String>?,
-    @ColumnInfo(name = "isProfilePublic") var isProfilePublic: Boolean
+    @get: Exclude @ColumnInfo(name = "friendsList") var friendList: List<String>?,
+    @get: Exclude @ColumnInfo(name = "isProfilePublic") var isProfilePublic: Boolean
 ){
     constructor() : this("", "", "","","","",0, emptyList(), true)
 }
